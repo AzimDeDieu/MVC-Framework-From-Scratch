7c9437e66f580c1433f0341e5f28c112bed77570
saz <saibouazim90@gmail.com>
2024-07-12 17:27:13 +0100

    Implémentation de Modeles/Postes.php pour récupérer les postes de la base de données sous forme de tableau

--- Application/Modeles/Postes.php
+++ Application/Modeles/Postes.php
@@ -1,9 +1,22 @@
 <?php
 
 namespace Application\Modeles; 
-use \Noyau\Modele;
+use \Noyau\Modele; 
+use \PDO;
+use \PDOException;
 
 class Postes extends Modele
 {
-
+    public static function getPostes(){
+        try{
+            $php_connecte_a_mysql = self::getConnexionEntrePhpEtMysql();
+            $instruction_mysql = $php_connecte_a_mysql->query("SELECT id, titre, contenu FROM postes ORDER BY created_at");
+            
+            $postes = $instruction_mysql->fetchAll(PDO::FETCH_ASSOC);
+            
+            return $postes;
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
+    }
 }
